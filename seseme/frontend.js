115954283b5a016deb87b37bfe361f6415a6d1ee
ettubrutesbro <Jack.Leng@gmail.com>
2014-11-12 02:49:35 -0800

    static 900x1600 vb

--- seseme/frontend.js
+++ seseme/frontend.js
@@ -19,51 +19,66 @@ Snap.load("sesemeiso2.svg", function(svgFile){
 
 
 
-	var a = g.select("#A_pillar")
-	var b = g.select("#B_pillar")
-	var c = g.select("#C_pillar")
-	var d = g.select("#D_pillar")
+	var a = g.select("#a")
+	var b = g.select("#b")
+	var c = g.select("#c")
+	var d = g.select("#d")
 
 	
 
 	//amount, speed, delay...finally modifies a global value var per pillar
 
-	var maska = g.select("#maska").select("#maskA")
-	var maskb = g.select("#maskb").select("#maskB")
-	var maskc = g.select("#maskc").select("#maskC")
-	var maskd = g.select("#maskd").select("#maskD")
 
+	var oldOffsetValue
 	var selectedPillar
 
 
-	movePillar(a, maska, 500, 30)
-	selectPillar(a, "#A_body", 600)
 	
+	
+	
+	a.click(function(){
+		selectPillar(a, 450)
+	})
+	
+	b.click(function(){
+		selectPillar(b, 450)
+	})	
+	c.click(function(){
+		selectPillar(c, 450)
+	})	
+	d.click(function(){
+		selectPillar(d, 450)
+	})
 
-
-	function unselectPillars(pillar1, pillar2, pillar3){
-		for(var i = 1; i<4; i++){
-			console.log(i)
-			var stroker = "pillar" + i
-
-		}
-	}
-
-	function selectPillar(pillar, body, speed){
-		//var stroker = pillar.select(body)
-		stroker.animate({
-			strokeDashoffset: 0
+	function unselectPillars(speed){
+		selectedPillar.animate({
+			strokeDashoffset: oldOffsetValue
 		}, speed)
 	}
 
-	function movePillar(pillar, mask, amount, delay){
-		//set delay 
-		
-		var mask = "mask" + pillar
-		
-		console.log(pillar)
-		console.log(mask)
+	function selectPillar(pillar, speed){
 
+		
+
+		if (selectedPillar!=undefined){
+			unselectPillars(450)
+		}
+
+		var ltr = pillar.attr('id')
+		var stroker = pillar.select('#' + ltr + '_body')
+		oldOffsetValue = stroker.attr('strokeDashoffset')
+		console.log(oldOffsetValue)
+		stroker.animate({
+			strokeDashoffset: 0
+		}, speed)
+
+		selectedPillar = stroker
+	}
+
+	function movePillar(pillar, amount, delay){
+		//set delay 
+		var ltr = pillar.attr('id')
+		var mask = g.select("#mask" + ltr ).select("#m" + ltr)
 
 		pillar.animate({
 			transform: "t 0 " + amount
